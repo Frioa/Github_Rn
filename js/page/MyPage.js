db213db1e57119bad3541553b47499cf3157d81e
Frioa <916378508@qq.com>
2019-06-03 22:54:17 +0800

    我的页面设计，ScrollView 的使用，公共组件封装 每一个Item使用工具类复用，

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -1,68 +1,167 @@
 import React, {Component} from 'react';
-import {Button, Platform, StyleSheet, Text, View} from 'react-native';
+import {Button, ScrollView, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import NavigationUtil from "../navigator/NavigationUtil";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Feather from "react-native-vector-icons/Feather";
+import {onThemeChange} from "../action/theme";
+import NavigationBar from "../common/NavigationBar";
+import {MORE_MENU} from "../common/MORE_MENU";
+import {connect} from 'react-redux';
+import GlobalStyles from "../res/GlobalStyles";
+import ViewUtil from "../util/ViewUtil";
 
-
+const THEME_COLOR = '#678';
 type Props = {};
 export default class MyPage extends Component<Props> {
-  render() {
-    const {navigation} = this.props;
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>MyPage</Text>
-        <Button
-            title={"改变主题颜色"}
-            onPress={() => {
-              navigation.setParams({  /*设置属性*/
-                theme: {
-                  tintColor: 'blue',
-                  updateTime: new Date().getTime()
-                }
-              })
-            }}
-        />
-       {/*   <Text onPress={() => {
-              NavigationUtil.goPage({
-                  navigation: this.props.navigation
-              }, "DetailPage")
-          }}>跳转到详情页</Text>
-          <Button
-              title={"Fetch 使用"}
-              onPress={() => {
-                  NavigationUtil.goPage({
-                      navigation: this.props.navigation
-                  }, "FetchDemoPage")
-              }}/>
-          <Button
-              title={"AsyncStorage 使用"}
-              onPress={() => {
-                  NavigationUtil.goPage({
-                      navigation: this.props.navigation
-                  }, "AsyncStorageDemoPage")
-              }}/>
-          <Button
-              title={"离线缓存框架使用"}
-              onPress={() => {
-                  NavigationUtil.goPage({
-                      navigation: this.props.navigation
-                  }, "DataStoreDemoPage")
-              }}/>*/}
-      </View>
-    );
-  }
+    getRightButton() {
+        return <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+            >
+                <View syles={{padding: 5, marginRight: 8}}>
+                    <Feather
+                        name={'search'}
+                        size={24}
+                        style={{color: 'white'}}/>
+                </View>
+            </TouchableOpacity>
+        </View>
+    }
+
+    getLeftButton(callBack) {
+        return <TouchableOpacity
+            style={{padding: 8, paddingLeft: 12}}
+            onPredd={callBack}>
+            <Ionicons
+                name={'ios-arrow-back'}
+                size={26}
+                style={{color: 'white'}}/>
+        </TouchableOpacity>
+    }
+
+    onClick(menu) {
+
+    }
+    getItem(menu) {
+        return ViewUtil.getMenuItem(()=>this.onClick(menu), menu, THEME_COLOR)
+    }
+    render() {
+        const {navigation} = this.props;
+        let statusBar = {
+            backgroundColor: THEME_COLOR,
+            barStyle: 'light-content',
+        };
+        let navigationBar =
+            <NavigationBar
+                title={'我的'}
+                statusBar={statusBar}
+                style={{backgroundColor: THEME_COLOR}}
+                rightButton={this.getRightButton()}
+                leftButton={this.getLeftButton()}/>;
+        return (
+            <View style={GlobalStyles.root_container}>
+                {navigationBar}
+                <ScrollView>
+                    <TouchableOpacity
+                        style={styles.item}
+                        onPress={() => this.onClick(MORE_MENU.About)}>
+                        <View style={styles.about_left}>
+                            <Ionicons
+                                name={MORE_MENU.About.icon}
+                                size={40}
+                                style={{
+                                    marginRight: 10,
+                                    color: THEME_COLOR,
+                                                                }}/>
+                            <Text>GitHub Popular</Text>
+                        </View>
+                        <Ionicons
+                        name={'ios-arrow-forward'}
+                        size={16}
+                        style={{
+                            marginRight: 10,
+                            alignSelf: 'center',
+                            color: THEME_COLOR,
+                        }}/>
+                    </TouchableOpacity>
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Tutorial)}
+                    {/*趋势管理*/}
+                    <Text style={styles.groupTitle}>趋势管理</Text>
+                    {/*自定义语言*/}
+                    {this.getItem(MORE_MENU.Custom_Language)}
+                    {/*语言排序*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Sort_Language)}
+
+                    {/*最热管理*/}
+                    <Text style={styles.groupTitle}>最热管理</Text>
+                    {/*自定义标签*/}
+                    {this.getItem(MORE_MENU.Custom_Key)}
+                    {/*标签排序*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Sort_Key)}
+                    {/*标签移除*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Remove_Key)}
+
+                    {/*设置*/}
+                    <Text style={styles.groupTitle}>设置</Text>
+                    {/*自定义主题*/}
+                    {this.getItem(MORE_MENU.Custom_Theme)}
+                    {/*关于作者*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.About_Author)}
+                    <View style={GlobalStyles.line}/>
+                    {/*反馈*/}
+                    {this.getItem(MORE_MENU.Feedback)}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.CodePush)}
+                </ScrollView>
+            </View>
+        );
+    }
 }
+/*
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({
+    onThemeChange: (theme) => dispatch(onThemeChange(theme))
+
+});
+export default connect(mapStateToProps, mapDispatchToProps)(MyPage)
+*/
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+
+    },
+    about_left: {
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    item: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+    groupTitle: {
+        marginLeft: 10,
+        marginTop: 10,
+        marginBottom: 5,
+        fontSize: 12,
+        color: 'gray',
+    }
 
 });
---
+++ js/res/GlobalStyles.js
@@ -0,0 +1,16 @@
+/**
+ * 全局样式
+ * @type {string}
+ */
+const BACKGROUND_COLOR = '#F3F3F3';
+export default {
+    line: {
+        height: 0.5,
+        opacity: 0.5,
+        backgroundColor: 'darkgray'
+    },
+    root_container: {
+        flex: 1,
+        backgroundColor: BACKGROUND_COLOR
+    }
+}
